0feef0e7d8159faa409677d18e35868139688293
xklnono <xklnono@126.com>
2018-05-26 17:18:02 +0800

    test travis

---
+++ module/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+@Brief entrance of program
+"""
+import subprocess
+import sys
+import time
+import ConfigParser
+import json
+import urllib
+import urllib2
+import MysqlHelper as mysql_helper
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        model = sys.argv[1]
+        batch_size = sys.argv[2]
+    else:
+        print "xxxxxxxxxxxxinput error!!!!xxxxxxxxxxxx"
